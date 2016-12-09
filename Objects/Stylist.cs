f89e5abad693842c1f0e2e4fff49118012170dc2
Anne Belka <antastic461@gmail.com>
2016-12-09 10:52:23 -0800

    add test for database empty at first for stylist

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 
-namespace HairSalon.Objects
+namespace HairSalon
 {
   public class Stylist
   {
@@ -16,6 +16,35 @@ namespace HairSalon.Objects
       this.Name = name;
       this.Availability = availability;
       this.Id = id;
+    }
+
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        string stylistAvailability = rdr.GetString(1);
+        Stylist newStylist = new Stylist(stylistName, stylistAvailability, stylistId);
+        allStylists.Add(newStylist);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+      return allStylists;
     }
   }
 }
---
+++ Tests/TestStylist.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class StylistTest// : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      int result = Stylist.GetAll().Count;
+      Assert.Equal(0, result);
+    }
+  }
+}
